3ddf0ed5ce2da42225297a8d96f2eddad39244fb
Hannahvic <hannahvl1987@hotmail.com>
2021-11-30 21:15:22 +0000

    main styling

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -2,16 +2,22 @@ import './main.css';
 import logo from '../../assets/images/logo.png';
 import wavy from '../../assets/images/wavy.png';
 import nz from '../../assets/images/nz.png';
+import half from '../../assets/images/half-circles.png';
 
 function Main() {
   return (
-    <div className='m-auto mt-11 main flex items-center justify-around rounded-lg'>
-      <div className='main-text flex'>
-        <h2 className='mr-6 flex flex-col '>
-          Hello,
-          <span>I'm a Developer</span>
-        </h2>
-        <img src={nz} alt='Picture' />
+    <div className='container'>
+      <div className='m-auto mt-11 main flex items-center justify-around rounded-lg'>
+        <div className='main-text flex'>
+          <h2 className='mr-12 text-3xl flex flex-col justify-center items-start'>
+            Hello,
+            <span>I'm a Developer</span>
+          </h2>
+          <img src={nz} alt='Picture' />
+        </div>
+      </div>
+      <div className='half-container'>
+        <img src={half} alt='green half circles' className='half' />
       </div>
     </div>
   );
