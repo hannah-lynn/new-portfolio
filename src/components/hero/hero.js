d12e307256065e2f6a18caf7b6bd4886fee23b16
Hannahvic <hannahvl1987@hotmail.com>
2021-12-02 08:13:16 +0000

    hero styling

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -3,14 +3,22 @@ import logo from '../../assets/images/logo.png';
 import wavy from '../../assets/images/wavy.png';
 import nz from '../../assets/images/nz.png';
 import half from '../../assets/images/half-circles.png';
+import halfLg from '../../assets/images/lg-half-circ.png';
 import lines from '../../assets/images/lines.png';
 
 function Hero() {
   return (
     <div className='hero'>
+      <div className='half-container'>
+        <img
+          src={halfLg}
+          alt='green half circle with red dot'
+          className='red-dot'
+        />
+      </div>
       <div className='m-auto mt-20 main flex items-center justify-around rounded-lg'>
-        <div className='main-text flex'>
-          <h2 className='mr-12 text-3xl flex flex-col justify-center items-start'>
+        <div className='main-text flex w-full justify-center space-x-24'>
+          <h2 className='text-3xl flex flex-col justify-center items-start'>
             Hello,
             <span>I'm a Developer</span>
           </h2>
