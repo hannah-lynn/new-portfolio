d68e2b9885d432f82ba625fc09a2e7f4d484ecef
Hannahvic <hannahvl1987@hotmail.com>
2021-12-03 08:04:17 +0000

    updating basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename='/'>
       <App />
     </HashRouter>
   </React.StrictMode>,
