0fab495199773e7f9e8e00cae2b2f1188937ba1f
Hannahvic <hannahvl1987@hotmail.com>
2021-12-26 14:15:43 +0000

    Toggle accessibility

--- src/components/darkmode/darkmode.js
+++ src/components/darkmode/darkmode.js
@@ -15,16 +15,18 @@ const DarkMode = () => {
     <div className='mode-toggle flex items-center mr-4'>
       <div className='flex items-center justify-center w-full mb-12'>
         <label
-          htmlFor='darkMode-toggle'
           className='flex items-center cursor-pointer'
+          htmlFor='darkMode-toggle'
+          aria-label='darkMode-toggle'
         >
           <div className='relative'>
             <input
               type='checkbox'
-              id='darkMode-toggle'
               name='darkMode-toggle'
+              id='darkMode-toggle'
               className='sr-only'
               onClick={handleOnClick}
+              aria-checked='true'
             />
             <div className='toggle-area block w-14 h-8 rounded-full' />
             <div className='dot absolute left-1 top-1 w-6 h-6 rounded-full transition' />
