6f5f24481621d54afa1b7ae02e2b555c3145da9c
Hannahvic <hannahvl1987@hotmail.com>
2022-01-02 17:47:37 +0000

    improving about me section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,25 +4,47 @@ function About() {
   return (
     <div className='flex flex-col mt-24 m-auto w-4/5' id='about'>
       <h1 className='font-bold text-xl mb-4'>About Me</h1>
-      <p className='text-left'>
-        I'm a self taught developer from Manchester, UK. I started teaching
-        myself HTML, CSS, and Javascript in 2019, when I was in New Zealand, and
-        was really happy to start a full time developer role in October 2020.
-        <br />
-        Currently, I work frontend with Angular and Typescript and backend with
-        PecoTS and MySQL. I enjoying coding in my spare time, where I build
-        things, like this website, using React. I'm constantly looking for new
-        things to learn and better ways to write my code, that's part of why I
-        love coding.
-        <br />
-        Previously, I worked in retail management, for Tesco in the UK and Urban
-        Outfitters in Berlin, Germany. I learnt a lot here about customers,
-        growing business, working to targets and leading teams.
-        <br />
-        I'm also a volunteer for Care4Calais, a volunteer run refugee charity
-        where I help give direct aid to asylum seekers in the UK. In my spare
-        time I like to grow houseplants.
-      </p>
+      <div className='grid grid-cols-2 md:gap-4'>
+        <div className='text-left col-span-2 md:col-span-1'>
+          <p>
+            I'm a self taught developer from Manchester, UK. I started teaching
+            myself HTML, CSS, and Javascript in 2019, when I was in New Zealand,
+            and was really happy to start a full time developer role in October
+            2020.
+          </p>
+          <p className='mt-2'>
+            Currently, I work frontend with Angular and Typescript and backend
+            with PecoTS and MySQL. I enjoying coding in my spare time, where I
+            build things, like this website, using React. I'm constantly looking
+            for new things to learn and better ways to write my code, that's
+            part of why I love coding.
+          </p>
+          <p className='mt-2'>
+            Previously, I worked as a Deputy Store Manager of a 60,000sq ft
+            Tesco Extra, with a turnover of £500k a week, and also at Urban
+            Outfitters in Berlin. Working in retail management has given me a
+            great perspective.
+          </p>
+        </div>
+        <p className='text-left col-span-2 md:col-span-1'>
+          <p className='mt-2'>
+            I'm always thinking of the customer journey - how to drive sales,
+            footfall, reduce loss and also working together, leading a team to
+            exceed targets.
+          </p>
+          <p className='mt-2'>
+            In my spare time I like to grow houseplants and travel. I've lived
+            and worked in 5 countries and travelled many more. One of my
+            favorite memories is climbing Volcan Acatenango, in Guatemala. We
+            camped 4000m up and watched the neighbouring Volcano, Fuego, erupt
+            every 10 minutes!
+          </p>
+          <p className='mt-2'>
+            I'm also a volunteer for Care4Calais, a volunteer run refugee
+            charity where I help give direct aid to asylum seekers in the UK.
+          </p>
+        </p>
+      </div>
     </div>
   );
 }
