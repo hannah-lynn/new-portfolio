820da064134b039ba98dded0c8dfb94c05cdd3a3
Hannah <hannahlynn.dev@gmail.com>
2022-02-08 10:23:31 +0000

    feat: projects section update

--- src/components/project card/project-card.js
+++ src/components/project card/project-card.js
@@ -1,9 +1,9 @@
 import { HashLink as Link } from 'react-router-hash-link';
 import './project-card.css';
 
-const ProjectCard = ({ img, title, description, href, link }) => {
+const ProjectCard = ({ img, title, description, view, github }) => {
   return (
-    <div className='card md:w-1/3 lg:w-1/4 transform flex flex-col rounded-lg shadow-2xl mb-6 mx-2'>
+    <div className='card sm:w-1/3 flex flex-col rounded-lg shadow-2xl mb-6 mx-2'>
       <img
         src={img}
         alt='screenshot of chat bot'
@@ -14,18 +14,29 @@ const ProjectCard = ({ img, title, description, href, link }) => {
 
       <h2 className='title font-bold my-3'>{title}</h2>
       <p className='mx-2 flex-1'>{description}</p>
-      {href ? (
-        <a
-          className='projects-btn rounded-2xl mx-auto my-3 cursor-pointer bg-gray-50 p-2'
-          href={href}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          {' '}
-          GitHub
-        </a>
+      {view && github ? (
+        <div className='buttons flex'>
+          <a
+            className='projects-btn rounded-2xl mx-auto my-3 cursor-pointer bg-gray-50 p-2'
+            href={view}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {' '}
+            View
+          </a>
+          <a
+            className='projects-btn rounded-2xl mx-auto my-3 cursor-pointer bg-gray-50 p-2'
+            href={github}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {' '}
+            Details
+          </a>
+        </div>
       ) : (
-        <Link to={link}>
+        <Link to={view}>
           <button className='projects-btn rounded-2xl mx-auto my-3 cursor-pointer bg-gray-50 p-2'>
             Details
           </button>
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,33 +1,35 @@
 import './projects.css';
 import essay from '../../assets/images/essay-ss.webp';
-import plant from '../../assets/images/plant-shop.webp';
-import calculator from '../../assets/images/calculator.webp';
-import countries from '../../assets/images/countries.webp';
-import rps from '../../assets/images/rps.webp';
-import tictactoe from '../../assets/images/tictactoe.webp';
 import chatbot from '../../assets/images/chat-bot.webp';
 import aloe from '../../assets/images/aloe-you.webp';
-import bill from '../../assets/images/bill.webp';
+import sport from '../../assets/images/sport-culture.webp';
 import ProjectCard from '../project card/project-card';
 
 function Projects() {
   return (
-    <div className='flex flex-col my-10 m-auto w-5/6' id='projects'>
-      <h1 className='font-bold text-xl my-4'>Projects</h1>
-      <div className='card-container justify-center md:flex flex-wrap mt-10 w-full'>
+    <div className='flex flex-col my-10 m-auto w-4/5' id='projects'>
+      <h1 className='font-bold text-xl my-4'>Featured Projects</h1>
+      <div className='card-container justify-center sm:flex flex-wrap mt-10'>
+        <ProjectCard
+          img={sport}
+          title={'Sport Culture'}
+          description={`A sport shop built using Typescript/Angular. Items are fetched from an api and can be added and removed from the basket`}
+          view={`https://github.com/hannah-lynn/sport-culture`}
+        />
         <ProjectCard
           img={aloe}
           title={'Aloe You'}
           description={`A WIP plant shop. I'm building this to improve my React skills,
             learn Firebase, experiment with GraphQL, Hooks and Redux.`}
-          href={`https://github.com/hannah-lynn/aloe-you`}
+          view={`https://hannah-lynn.github.io/aloe-you/`}
+          github={`https://github.com/hannah-lynn/aloe-you`}
         />
         <ProjectCard
           img={chatbot}
           title={'Chat Bot'}
           description={`This chat bot was built using Typescript, Angular with Tailwind and
             CSS. Please click to see a video of the bot working.`}
-          link={`/chat-bot`}
+          view={`/chat-bot`}
         />
         <ProjectCard
           img={essay}
@@ -35,48 +37,55 @@ function Projects() {
           description={`A site to display essays and link back to criteria or reference
             points in the essay. Made using React and React Tabs and styled with
             Tailwind CSS.`}
-          href={`https://github.com/hannah-lynn/essay-site`}
+          view={`https://hannah-lynn.github.io/essay-site`}
+          github={`https://github.com/hannah-lynn/essay-site`}
         />
-        <ProjectCard
+        {/* <ProjectCard
           img={calculator}
           title={'Calculator'}
           description={`A simple calculator built using HTML, CSS and Javascript.`}
-          href={`https://github.com/hannah-lynn/calculator`}
+          view={`https://hannah-lynn.github.io/calculator`}
+          github={`https://github.com/hannah-lynn/calculator`}
         />
         <ProjectCard
           img={plant}
           title={'Plant Shop'}
           description={`A fully responsive single page website built using HTML, CSS and
             Javascript. Showcasing an animated nav on smaller screens.`}
-          href={`https://github.com/hannah-lynn/theplantshop`}
+          view={`https://hannah-lynn.github.io/theplantshop`}
+          github={`https://github.com/hannah-lynn/theplantshop`}
         />
         <ProjectCard
           img={countries}
           title={'Countries'}
           description={`Using Rest Countries API you can search through to find your country
             of choice with Capital City and flag.`}
-          href={`https://github.com/hannah-lynn/countries`}
+          view={`https://hannah-lynn.github.io/countries`}
+          github={`https://github.com/hannah-lynn/countries`}
         />
         <ProjectCard
           img={rps}
           title={'Rock, Paper, Scissors'}
           description={`A cute rock paper scissors game that can be played against the
             computer on smaller and larger screens.`}
-          href={`https://github.com/hannah-lynn/rockpaperscissors`}
+          view={`https://hannah-lynn.github.io/rockpaperscissors`}
+          github={`https://github.com/hannah-lynn/rockpaperscissors`}
         />
         <ProjectCard
           img={tictactoe}
           title={'Tick Tac Toe'}
           description={`Tic Tac Toe can is a responsive 2 player game utilising simple
             javascript is along with HTML and CSS.`}
-          href={`https://github.com/hannah-lynn/tictactoe`}
+          view={`https://hannah-lynn.github.io/tictactoe`}
+          github={`https://github.com/hannah-lynn/tictactoe`}
         />
         <ProjectCard
           img={bill}
           title={'Bill Splitter'}
           description={`Bill splitting application build using HTML, CSS and JS, allows users to type in bill total, add service, and split between diners`}
-          href={`https://github.com/hannah-lynn/billsplitter`}
-        />
+          view={`https://hannah-lynn.github.io/billsplitter`}
+          github={`https://github.com/hannah-lynn/billsplitter`}
+        /> */}
       </div>
     </div>
   );
